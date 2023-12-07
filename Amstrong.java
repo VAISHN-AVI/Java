1b0800155c2e4f34a3be06641c4a5dc037efa44d
VAISHN-AVI <145766012+VAISHN-AVI@users.noreply.github.com>
2023-12-07 21:52:40 +0530

    Add files via upload

---
+++ Amstrong.java
@@ -0,0 +1,20 @@
+class Amstrong
+{
+	public static void main(String[] args) {
+		
+		int num=153;
+
+		int count++;
+		for(int i=num; i>0 ;i/=10)
+		{
+			count++;
+		}
+
+		for(int j=num; j>0 ;j/=10)
+		{
+			int rem=j%10;
+
+			for(int i=0; i)
+		}
+	}
+}
